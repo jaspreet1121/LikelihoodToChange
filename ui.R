fdf266d7060e55ae0b6a067d9f0d6030593f9b77
jaspreet1121 <jaspreet1141@hotmail.com>
2017-03-31 11:19:22 +0530

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+fluidPage(
+  titlePanel("Computing the likelihood to change score"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('file1', 'Choose CSV file of Linkedin connections',
+                accept=c('text/csv', 
+                         'text/comma-separated-values,text/plain', 
+                         '.csv')),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ','),
+      radioButtons('quote', 'Quote',
+                   c(None='',
+                     'Double Quote'='"',
+                     'Single Quote'="'"),
+                   '"')
+    ),
+    mainPanel(
+      tableOutput('contents')
+    )
+  )
+)
+  
